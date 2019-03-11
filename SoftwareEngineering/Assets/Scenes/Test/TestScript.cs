761de374405a38425e47999a7c982d50bf89f37c
ShadowSneaker <danielsfoyle@gmail.com>
2019-03-11 17:23:03 +0000

    Started Room changing
    Started seperating the rooms so that they can be level streamed easily.

--- SoftwareEngineering/Assets/Scenes/Test/TestScript.cs
+++ SoftwareEngineering/Assets/Scenes/Test/TestScript.cs
@@ -15,30 +15,39 @@ public class TestScript : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        SceneManager.LoadScene("Test1", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Test2", LoadSceneMode.Additive);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
 		if (ToggleLevel1 && Level1On)
         {
             Level1On = false;
-            //SceneManager.
+            SceneManager.LoadScene("Test1", LoadSceneMode.Additive);
+            Debug.Log("Opened Test1");
         }
         else if (!ToggleLevel1 && !Level1On)
         {
             Level1On = true;
+            SceneManager.UnloadSceneAsync("Test1");
+            Debug.Log("Closed Test1");
         }
 
 
         if (ToggleLevel2 && Level2On)
         {
-            Level1On = false;
+            Level2On = false;
+            SceneManager.LoadScene("Test2", LoadSceneMode.Additive);
+            Debug.Log("Opened Test2");
         }
         else if (!ToggleLevel2 && !Level2On)
         {
             Level2On = true;
+            SceneManager.UnloadSceneAsync("Test2");
+            Debug.Log("Closed Test2");
         }
     }
 }
