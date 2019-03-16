2a3d172d2d50bbf1405ea7e97d655b7759059bfe
ShadowSneaker <danielsfoyle@gmail.com>
2019-03-16 01:36:33 +0000

    Added Level Streaming
    Rooms now load and unload while traversing the mansion to reduce lag.
    Some rooms may make the game stutter a bit as a lot of rooms may be loading / unloading. the only room noted that does this is the Foyer.

---
+++ SoftwareEngineering/Assets/Scripts/RoomArchive.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomArchive : MonoBehaviour
+{
+    public string[] Keys;
+    public GameObject[] Values;
+
+    private Dictionary<string, GameObject> Rooms = new Dictionary<string, GameObject>();
+
+
+    private void Start()
+    {
+        if (Keys.Length == Values.Length)
+        {
+            for (int i = 0; i < Keys.Length; ++i)
+            {
+                Rooms.Add(Keys[i], Values[i]);
+            }
+        }
+        else
+        {
+            Debug.LogError("Error: The amount of keys does not match the amount of values.");
+        }
+    }
+
+
+    public void OpenRooms(string[] RoomList)
+    {
+        GameObject Room;
+        for (int i = 0; i < RoomList.Length; ++i)
+        {
+            if (Rooms.TryGetValue(RoomList[i], out Room))
+            {
+                if (!Room.activeInHierarchy)
+                {
+                    Room.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void CloseRooms(string[] RoomList)
+    {
+        GameObject Room;
+        for (int i = 0; i < RoomList.Length; ++i)
+        {
+            if (Rooms.TryGetValue(RoomList[i], out Room))
+            {
+                if (Room.activeSelf)
+                {
+                    Room.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ SoftwareEngineering/Assets/Scripts/RoomLoader.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class RoomLoader : MonoBehaviour
+{
+    // WARNING - Only use this for the base scene, Not any other scene.
+    // Creates a scene on startup.
+    //public string StartRoom;
+
+    // Unloads all open scenes in the game.
+    //public bool ShouldUnloadAllRooms;
+
+    // A list of rooms that will be opened.
+    public string[] LoadRooms;
+
+    // A list of rooms that will be closed.
+    public string[] UnloadRooms;
+
+
+    private RoomArchive Archive;
+
+
+    /// Functions
+
+    private void Awake()
+    {
+        Archive = FindObjectOfType<RoomArchive>();
+
+        //if (StartRoom != "")
+        //{
+        //    SceneManager.LoadScene(StartRoom, LoadSceneMode.Additive);
+        //}
+    }
+
+
+
+    //public void InitiateStream()
+    //{
+    //    // Load new rooms.
+    //    for (int i = 0; i < LoadRooms.Length; ++i)
+    //    {
+    //        if (!CheckSceneLoaded(LoadRooms[i]))
+    //        {
+    //            SceneManager.LoadScene(LoadRooms[i], LoadSceneMode.Additive);
+    //        }
+    //    }
+    //
+    //
+    //    // Unload old rooms.
+    //    for (int i = 0; i < UnloadRooms.Length; ++i)
+    //    {
+    //        if (CheckSceneLoaded(LoadRooms[i]))
+    //        {
+    //            SceneManager.UnloadSceneAsync(UnloadRooms[i]);
+    //            Debug.Log(UnloadRooms[i]);
+    //        }
+    //    }
+    //}
+
+
+    // Returns true if the specified scene is open.
+    //protected bool CheckSceneLoaded(string Scene)
+    //{
+    //    for (int i = 0; i < SceneManager.sceneCount; ++i)
+    //    {
+    //        if (SceneManager.GetSceneAt(i).name == Scene)
+    //        {
+    //            return true;
+    //        }
+    //    }
+    //    return false;
+    //}
+
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        if (Other.CompareTag("Player") && Archive)
+        {
+            Archive.OpenRooms(LoadRooms);
+            Archive.CloseRooms(UnloadRooms);
+        }
+
+        //if (ShouldUnloadAllRooms)
+        //{
+        //    for (int i = 0; i < SceneManager.sceneCount; ++i)
+        //    {
+        //        SceneManager.UnloadSceneAsync(i);
+        //    }
+        //}
+        //else
+        //{
+        //    InitiateStream();
+        //}
+    }
+}
