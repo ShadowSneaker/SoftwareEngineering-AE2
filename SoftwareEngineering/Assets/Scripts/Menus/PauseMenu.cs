1c935359180d66a2d0cae4811e44836af2777261
Byron <byronandrews-salvietto@live.co.uk>
2019-03-19 16:52:22 +0000

    UI
    Sanity and health Bar actually work

--- SoftwareEngineering/Assets/Scripts/Menus/PauseMenu.cs
+++ SoftwareEngineering/Assets/Scripts/Menus/PauseMenu.cs
@@ -1,48 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PauseMenu : MonoBehaviour
 {
-    public float HealthBarDisplay;
-    public Transform HealthBar;
-    public Texture2D EmptyHealthBar;
-    public Texture2D FullHealthBar;
 
-    public float SanityBarDisplay;
-    public Transform SanityBar;
-    public Texture2D EmptySanityBar;
-    public Texture2D FullSanityBar;
-
-    private void OnGUI()
-    {
-        //draw the backgroud
-        GUI.BeginGroup(new Rect(new Vector2(HealthBar.position.x, HealthBar.position.y), new Vector2(HealthBar.localScale.x, HealthBar.localScale.y)));
-        GUI.Box(new Rect(new Vector2(0, 0), new Vector2(HealthBar.localScale.x, HealthBar.localScale.y)), EmptyHealthBar);
-
-        //draw the filled in part
-        GUI.BeginGroup(new Rect(new Vector2(HealthBar.position.x, HealthBar.position.y), new Vector2(HealthBar.localScale.x * HealthBarDisplay, HealthBar.localScale.y)));
-        GUI.Box(new Rect(new Vector2(0, 0), new Vector2(HealthBar.localScale.x, HealthBar.localScale.y)), FullHealthBar);
-
-
-        //draw the backgroud
-        GUI.BeginGroup(new Rect(new Vector2(SanityBar.position.x, SanityBar.position.y), new Vector2(SanityBar.localScale.x, SanityBar.localScale.y)));
-        GUI.Box(new Rect(new Vector2(0, 0), new Vector2(SanityBar.localScale.x, SanityBar.localScale.y)), EmptySanityBar);
-
-        //draw the filled in part
-        GUI.BeginGroup(new Rect(new Vector2(SanityBar.position.x, SanityBar.position.y), new Vector2(SanityBar.localScale.x * SanityBarDisplay, SanityBar.localScale.y)));
-        GUI.Box(new Rect(new Vector2(0, 0), new Vector2(SanityBar.localScale.x, SanityBar.localScale.y)), FullSanityBar);
-
-
-        GUI.EndGroup();
-        GUI.EndGroup();
-        GUI.EndGroup();
-        GUI.EndGroup();
-    }
+    public Slider HealthBar;
+    public Slider SanityBar;
+    public PlayerScript Player;
 
 
     void Update ()
     {
-        HealthBarDisplay = GetComponent<PlayerScript>().CurrentHealth;
+        HealthBar.value = Player.CurrentHealth / Player.MaxHealth;
+        SanityBar.value = Player.Sanity / Player.MaxSanity;
+
 	}
 }
