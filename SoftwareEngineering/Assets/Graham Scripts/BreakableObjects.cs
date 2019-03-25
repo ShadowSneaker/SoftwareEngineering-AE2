72cdecfc2ffc0ce1b2ea89ccd564c105109cc8cf
ghillman <ghillman@live.co.uk>
2019-03-25 17:45:53 +0000

    breaking stuff and bottles done

---
+++ SoftwareEngineering/Assets/Graham Scripts/BreakableObjects.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObjects : MonoBehaviour {
+
+    public GameObject Particles;
+    public bool Thrown;
+
+
+	// Use this for initialization
+	void Start () {
+        Thrown = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Thrown)
+        {
+            GetComponent<Rigidbody>().isKinematic = false;         
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Thrown)
+        {
+            Break();
+        }
+    }
+    private void Break()
+    {
+        Instantiate(Particles, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+}
---
+++ SoftwareEngineering/Assets/Graham Scripts/ParticleBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBehaviour : MonoBehaviour {
+
+    private float CurrentTime;
+    
+	// Use this for initialization
+	void Start () {
+        CurrentTime = 0;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CurrentTime += Time.deltaTime;
+        if(CurrentTime >= 2)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
