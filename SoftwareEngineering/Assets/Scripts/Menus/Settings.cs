011a4978701901af0cfb8b830724b634b791baac
Byron <byronandrews-salvietto@live.co.uk>
2019-03-27 15:20:41 +0000

    Player added brigtness settings
    done

--- SoftwareEngineering/Assets/Scripts/Menus/Settings.cs
+++ SoftwareEngineering/Assets/Scripts/Menus/Settings.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Settings : MonoBehaviour
 {
-    
+    public GameObject TheLight;
 
-  
+    public float Brightness;
+    public Scrollbar SliderLocation;
 
-    public void ChangeControlls()
+
+    public void QuitGame()
     {
-       // nothing needs to be done as the default allows for the controll
+        Application.Quit();
     }
-	
-	
+
+
+    private void Update()
+    {
+        Brightness = SliderLocation.value;
+
+
+        foreach(Transform child in TheLight.transform)
+        {
+            child.GetComponent<Light>().intensity = Brightness;
+        }
+
+        
+
+        
+
+    }
+
+   
+
 }
