738b4b878d3a96bfc0cca5611cb82b5b87717783
ShadowSneaker <danielsfoyle@gmail.com>
2019-04-13 14:54:39 +0100

    Room Events along with adding the basement
    Basement added to the Mansion Scene
    Events done for all rooms (The cut scene events don't have cut scenes).
    Pizza guy dissapears on startup for some reason (doesn't chase player)

---
+++ SoftwareEngineering/Assets/Scripts/PlayCinamatic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCinamatic : MonoBehaviour
+{
+    public bool PlayOnce = true;
+    
+    // Cinamatic video thingie here.
+    public GameObject Cinamatic;
+    
+
+    private bool Played;
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        if (Other.CompareTag("Player"))
+        {
+            // Play Cinamatic
+            if (!Played && Cinamatic)
+            {
+                if (PlayOnce)
+                {
+                    Played = true;
+                }
+            }
+        }
+    }
+}
