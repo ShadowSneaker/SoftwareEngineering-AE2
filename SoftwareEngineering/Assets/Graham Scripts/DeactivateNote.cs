badb189b6146df6acb8197a6211c0f2f56931107
ghillman <ghillman@live.co.uk>
2019-04-15 15:42:00 +0100

    notes complete

---
+++ SoftwareEngineering/Assets/Graham Scripts/DeactivateNote.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateNote : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GetComponentInChildren<ActivatableObject>().Activated = false;
+        }
+    }
+}
---
+++ SoftwareEngineering/Assets/Graham Scripts/DisplayNotestext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayNotestext : MonoBehaviour {
+
+
+    public GameObject Text;
+    public string NoteText;
+    private bool InteractingWith;
+	// Use this for initialization
+	void Start () {
+        InteractingWith = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<ActivatableObject>().Activated == true)
+        {
+            Text.GetComponent<Text>().enabled = true;
+            Text.GetComponent<Text>().text = NoteText;
+            InteractingWith = true;
+        }
+        else if(InteractingWith && GetComponent<ActivatableObject>().Activated ==false)
+        {
+            InteractingWith = false;
+            Text.GetComponent<Text>().enabled = false;
+            Text.GetComponent<Text>().text = " ";
+        }
+	}
+    
+}
